ad622fd01e9c00bf16fe0762e906d46ecccf4dd0
Cassius Vonthill <cassiusvonthill@gmail.com>
2020-05-09 17:03:31 -0400

    dynamic port

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/Heart-plus-N/habitica-quest-queue-notifier
 
-go 1.13
+go 1.14
 
 require github.com/go-chi/chi v4.1.1+incompatible
--- habitica-quest-queue-notifier.go
+++ habitica-quest-queue-notifier.go
@@ -4,10 +4,18 @@ import (
 	"log"
 	"net/http"
 
+	"os"
+
 	"github.com/go-chi/chi"
 )
 
-const host string = ":3333"
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return ":" + port, nil
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -19,7 +27,11 @@ func main() {
 	// r.Post("/chat")
 	// r.Post("/quest")
 
+	host, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Println("Listening at: ", host)
-	err := http.ListenAndServe(host, r)
+	err = http.ListenAndServe(host, r)
 	log.Fatal(err)
 }

ed32de4c75474bb8f8639a6a1d3b11f5d046784b
Cassius Vonthill <cassiusvonthill@gmail.com>
2020-05-14 11:46:07 -0400

    :mute: :art: :heavy_minus_sign: comments & cleanup

--- main.go
+++ main.go
@@ -14,16 +14,14 @@ import (
 	"github.com/spf13/viper"
 
 	"github.com/go-chi/chi"
-	"github.com/goware/httplog"
 	. "gitlab.com/bfcarpio/gabit"
 )
 
 func main() {
-	// Load variable from config
+	// Load ENV or config variables
 	viper.SetDefault("PORT", ":8080")
 	if os.Getenv("ENV") == "PROD" {
 		log.Println("Loading environment variables")
-		viper.BindEnv("PORT")
 		viper.AutomaticEnv()
 	} else {
 		log.Println("Loading config file")
@@ -33,11 +31,13 @@ func main() {
 		viper.ReadInConfig()
 	}
 
+	// Ensure we have a port
 	port := ":" + viper.GetString("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
+	// Connect to habitica
 	hapi := NewHabiticaAPI(nil, "", nil)
 	_, err := hapi.Authenticate(viper.GetString("HABITICA_USERNAME"), viper.GetString("HABITICA_PASSWORD"))
 	if err != nil {
@@ -46,6 +46,7 @@ func main() {
 	}
 	log.Println("Logged into Habitica")
 
+	// Set up oversevers
 	reporter := op.Reporter{}
 
 	quest_queue := qq.QuestQueue{Name: "QQ"}
@@ -54,19 +55,11 @@ func main() {
 	quest_queue_2 := qq.QuestQueue{Name: "QQ2"}
 	reporter.Subscribe(quest_queue_2)
 
+	// Open routes
 	r := chi.NewRouter()
-
-	// Logger
-	logger := httplog.NewLogger("httplog-example", httplog.Options{
-		JSON: true,
-	})
-
-	r.Use(httplog.RequestLogger(logger))
-
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Bot up as of: %s", time.Now().String())
 	})
-
 	r.Post("/task", func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {

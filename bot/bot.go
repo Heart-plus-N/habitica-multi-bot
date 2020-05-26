114e75677b98fa4cf0a2865494b7bdbe2732784b
Cassius Vonthill <cassiusvonthill@gmail.com>
2020-05-26 19:48:12 -0400

    :bug: PostMessage to group that send message

--- bot/bot.go
+++ bot/bot.go
@@ -16,18 +16,26 @@ type Bot struct {
 
 func (b Bot) Initiate(at ActivityType, body []byte, sc SharedConfig) {
 	log.Println("Bot Utils")
+
+	// Get the new chat message
 	value, err := jsonparser.GetString(body, "unformattedText")
 	if err != nil {
 		log.Println("Couldn't parse body")
 		log.Println(err)
 	}
 
+	// Split the message so we can use it and identify the parts of
+	// a command.
 	valueSplit := strings.Fields(value)
 
 	if valueSplit[0] == "!ub" {
+		// Since we know it's a command we need to
+		// find a group to post the message in.
+		group, err := jsonparser.GetString(body, "group", "id")
+
 		api := gabit.NewHabiticaAPI(nil, "", nil)
 		api.Authenticate(sc.HabiticaUsername, sc.HabiticaPassword)
-		_, err := api.PostMessage("party", "Utility_Bot is up as of: "+time.Now().String())
+		_, err = api.PostMessage(group, "Utility_Bot is up as of: "+time.Now().String())
 		if err != nil {
 			log.Println(err)
 		}
